80f755644e6a15d5fefae2ec88d907eac5289340
Alex S <alex.sunga@fliptech.co>
2018-08-30 08:59:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,9 @@ function checkCollision(rock) {
                *    and the rock's right edge is > the DODGER's right edge
                ) {*/
       return (
-        (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >)
+        (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
+        (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
+        (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
         )
     }
   
